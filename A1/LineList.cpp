3beb1e0137e9a794c6064f3275270dc4dd81ca9a
Sadaf Najam <sadaf.najam91@gmail.com>
2019-05-28 22:15:52 -0400

    Implementation complete of LineList.cpp

--- A1/LineList.cpp
+++ A1/LineList.cpp
@@ -9,21 +9,172 @@
     #include <string>
 
     #include "LineList.h"
+    #include "Line.h"
 
     using std::cout;
     using std::endl;
 
     //Default constructor
     LineList::LineList(){
-        cout << "default ctor" << endl;
+        cout << "Default ctor" << endl;
+        head=new Node(Line{""}, nullptr, nullptr);
+        tail=new Node(Line{""}, nullptr, nullptr);
+        head->next=tail;
+        tail->prev=head;
     }
 
     //Destructor
     LineList::~LineList(){
         cout << "dtor" << endl;
-       // delete[] msgPtr;
-        //msgPtr = nullptr;
+        while (!empty()) {
+            pop_front();
+        }
+        delete [] head;
+        head=nullptr;
+        delete [] tail;
+        tail=nullptr;
     }
+
+    //Copy ctor
+    LineList::LineList(const LineList& rhs){
+        cout << "Copy ctor" << endl;
+        head=new Node(Line{""}, nullptr, nullptr);
+        tail=new Node(Line{""}, nullptr, nullptr);
+        head->next=tail;
+        theSize=rhs.size();
+        if (rhs.empty()) {
+            return;
+        }
+        Node* current=rhs.head->next;
+        while (current != rhs.tail) {
+            push_back(current->data);
+            current=current->next;
+        }
+    }
+
+    //Copy assignment
+    const LineList& LineList:: operator=(const LineList& rhs){
+        if (this==&rhs) {
+            return *this;
+        }
+        while (empty()!=1) {
+            pop_back();
+        }
+        LineList varname(rhs);
+        return *this;
+    }
+
+    //Inserts line at front of the list
+    void LineList::push_front(const Line& line){
+        Node* insert=new Node(line, head, head->next);
+        head->next->prev=insert;
+        head->next=insert;
+        theSize+=1;
+    }
+
+    //Inserts line at back of the list
+    void LineList::push_back(const Line& line){
+        Node* insert=new Node(line, tail->prev, tail);
+        tail->prev->next=insert;
+        tail->prev=insert;
+        theSize+=1;
+    }
+
+    //Removes first node from the list
+    void LineList::pop_front(){
+        Node* removal=head->next;
+        if(removal != tail){
+            removal->next->prev = head;
+            head->next = removal->next;
+            delete [] removal;
+            removal = nullptr;
+            theSize-= 1;
+        }
+    }
+
+
+    //Removes last node from the list
+    void LineList::pop_back(){
+        Node * removal = tail->prev;
+        if(removal != head){
+            removal->prev->next = tail;
+            tail->prev = removal->prev;
+            delete [] removal;
+            removal = nullptr;
+            theSize-= 1;;
+        }
+    }
+
+    //Returns the size of list
+    int LineList::size() const{
+        return theSize;
+    }
+
+    //Returns true or false if list is empty
+    bool LineList::empty() const{
+        bool isEqual = 0;
+        if((head->next == tail) && (tail->prev == head)){
+            isEqual = 1;
+        }
+        return isEqual;
+    }
+
+    //Inserts a new line at position k in this list
+    void LineList::insert(const Line& line, int k){
+        if(head->next==tail || size()<k){
+            cout<<"position out of bound"<<endl;
+        }
+        else{
+            if(k==1){
+                push_front(line);
+                return;
+            }
+            Node * newNode = head;
+            int count = 0;
+            while(count != k){
+                newNode = newNode->next;
+                count++;
+            }
+            Node * n = new Node{ line , newNode->prev , newNode};
+            newNode->prev->next = n;
+            newNode->prev = n;
+            theSize+= 1;
+            
+        }
+    }
+
+    //Removes node at position k in this list
+    void LineList::remove(int k){
+        if(head->next==tail || size()<k){
+            cout<<"position out of bound"<<endl;
+        }
+        else{
+            Node * removeNode = head;
+            int count = 0;
+            while(count != k){
+                removeNode = removeNode->next;
+                count++;
+            }
+            removeNode->prev->next = removeNode->next;
+            removeNode->next->prev = removeNode->prev;
+            theSize-= 1;
+            delete [] removeNode;
+            removeNode = nullptr;
+        }
+    }
+
+    //Returns the line at position k in this list
+    Line LineList::get (int k) const{
+        Node * node = head;
+        int count = 0;
+        while(count != k){
+            node = node->next;
+            count++;
+        }
+        return node->data;
+    }
+
+
 
     //Print manipulated list
     void LineList::print(){
@@ -37,6 +188,4 @@
         //return expression;
     }
 
-    LineList::LineList(const LineList& rhs){
-        
-    }
+
--- A1/LineList.h
+++ A1/LineList.h
@@ -6,6 +6,8 @@
     //  Copyright © 2019 Sadaf Najam. All rights reserved.
     //
 
+    #include "Line.h"
+
     #ifndef LineList_h
     #define LineList_h
 
@@ -18,7 +20,7 @@
             Line data;
             Node* prev;
             Node* next;
-            Node(const Line& In, Node* prv, Node* nxt);
+            Node(const Line& In, Node* prv, Node* nxt):data{In}, prev{prv}, next{nxt}{};
         };
         
         Node* head; //Pointer to the first node in this list
@@ -26,20 +28,32 @@
         
     public:
         LineList(); //Default ctor
+        
         virtual ~LineList(); //Dtor
+        
         explicit LineList(const LineList& rhs); //Copy ctor
+        
         const LineList& operator=(const LineList& rhs); //Copy assignment
         
-        void push_front(const Line& line);
-        void push_back(const Line& line);
-        void pop_front();
-        void pop_back();
-        int size() const;
-        bool empty() const;
-        void insert(const Line& line, int k);
-        void remove(int k);
-        Line get(int k) const;
-        void print();
+        void push_front(const Line& line); //Inserts line at the front of the this list
+        
+        void push_back(const Line& line); //Inserts line at the end of the this list
+        
+        void pop_front(); //Remove the first node in this list
+        
+        void pop_back(); //Remove the last node in this list
+        
+        int size() const; //Returns the size of this list
+        
+        bool empty() const; //Returns whether this list is empty
+        
+        void insert(const Line& line, int k); //Inserts a new line at position k in this list
+        
+        void remove(int k); //Removes node at position k in this list
+        
+        Line get(int k) const; //Returns the line at position k in this list
+        
+        void print(); //Prints the lineList
         
     };
 
